9cf372430dee3d08fb503a98230d40e8013993f5
German Franco F <933@holbertonschool.com>
2019-06-19 19:12:10 +0000

    correcting according to Betty Style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-	/*
-	*Print a String 
-	*/
+/*
+*print a string with puts
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-} 
+}
