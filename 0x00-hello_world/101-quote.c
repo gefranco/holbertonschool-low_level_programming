e34f6f778aa361301a809a44276c3b99382152a9
German Franco F <933@holbertonschool.com>
2019-06-19 20:41:31 +0000

    a C program that prints to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Fail)
  */
 int main(void)
 {
-	//printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
