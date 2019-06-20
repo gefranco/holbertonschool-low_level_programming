b4be39ddd56b89e9176276a553f4b4e02d6d1257
German Franco F <933@holbertonschool.com>
2019-06-20 15:48:37 +0000

    Print all the letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	while (alphabet[i] != '\0')
+	{
+		if (alphabet[i] != 'q' && alphabet[i] != 'e')
+		{
+			putchar(alphabet[i]);
+		}
+		i++;
+	}
+	putchar('\n');
+}
