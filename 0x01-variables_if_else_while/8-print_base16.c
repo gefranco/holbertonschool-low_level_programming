749552a852f7df6adf8cf48f60960d3bec56299b
German Franco F <933@holbertonschool.com>
2019-06-20 19:06:02 +0000

    rints all the numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alphabet[] = "123456789abcdef";
+	int i = 0;
+
+	while (alphabet[i] != '\0')
+	{
+		putchar(alphabet[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
