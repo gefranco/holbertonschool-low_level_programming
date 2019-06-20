713612dfe6ed0b9cf22b0de860d84bcc9874a3a7
German Franco F <933@holbertonschool.com>
2019-06-20 18:24:52 +0000

    print numbers whit putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	long int i = 0;
+
+	while (i < 100)
+	{
+		putchar(i / 10 + '0')
+		i += 10;
+	}
+	putchar('\n');
+	return (0);
+}
