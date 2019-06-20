95b2d1a83d5a57c49b9555f2764b0ce5d4af1a48
German Franco F <933@holbertonschool.com>
2019-06-20 16:01:26 +0000

    print the number from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
