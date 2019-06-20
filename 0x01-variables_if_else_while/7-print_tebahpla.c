c58b7dc6d1042678b1600ab53a5f47d7625ac956
German Franco F <933@holbertonschool.com>
2019-06-20 18:45:47 +0000

    alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = sizeof(alphabet);
+
+	while (i >= 0)
+	{
+		putchar(alphabet[i]);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
