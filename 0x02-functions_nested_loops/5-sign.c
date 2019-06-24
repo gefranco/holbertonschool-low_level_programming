5fe0b342e96dec765562627e3ec4cae67f540e94
German Franco F <933@holbertonschool.com>
2019-06-24 20:31:28 +0000

    a function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
 /**
-* print_sign - checks for lowercase character.
+* islower - checks for lowercase character.
 * @c: The character to be checked
 * Return: 1 if c is lowercase 0 otherwise
 */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
 	}
 	else
 	{
