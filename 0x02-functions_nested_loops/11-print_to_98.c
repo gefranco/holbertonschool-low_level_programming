d3fe7b8b0d028771f27a8bb8203a323f309d42ee
German Franco F <933@holbertonschool.com>
2019-06-25 15:11:15 +0000

    prints all natural numbers from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+* print_to_98 - prints all natural numbers from n to 98
+*/
+void print_to_98(int n)
+{
+	for (; n <= 98; n <= 98 ? n++ : n--)
+	{
+		if (n != 98)
+		{
+			printf("%d, ", n);
+		}
+		else
+		{
+			printf("%d", n);
+		}
+	}
+	printf("\n");
+}
