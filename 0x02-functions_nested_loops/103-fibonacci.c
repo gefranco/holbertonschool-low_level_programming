e252a7b74c2fd50b234848bf56a9cf6ebc66f558
German Franco F <933@holbertonschool.com>
2019-06-25 19:23:34 +0000

    fix Betty styles

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
 /**
-* main - computes and prints the sum of all the multiples of 3 or 5 below 1024
+* main - finds and prints the sum of the even-valued
+* Return: 0 for success
 */
-int main(int n)
+int main(void)
 {
 	long n1 = 1;
 	long n2 = 2;
 	long sum = 0;
 	int i;
 	long sumOdd = 0;
-	while(n2 < 4000000)
-	{
-		
-		sum = n1 + n2;
-		
-		n2=sum;
-		if(sum % 2 !=0)
-		{
-			sumOdd+=sum;
-		}
 
-		
+	while (n2 < 4000000)
+	{
+		sum = n1 + n2;
+		n2 = sum;
+		if (sum % 2 != 0)
+		{
+			sumOdd += sum;
+		}
 		printf("%ld ,", sumOdd);
 	}
-	
-
 	printf("%ld ,", sumOdd);
 	putchar('\n');
 	return (0);
