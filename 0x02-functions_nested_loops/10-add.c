635e8f7564b8fa0acb8364cd4514d4fe71b9f1b5
German Franco F <933@holbertonschool.com>
2019-06-25 19:05:37 +0000

    fix Betty styles

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,7 +3,7 @@
 * add - add two numbers
 * @n1: first number to add
 * @n2: second number to add
-* Return the result of sum the two numbers
+* Return: the result of sum the two numbers
 */
 int add(int n1, int n2)
 {
