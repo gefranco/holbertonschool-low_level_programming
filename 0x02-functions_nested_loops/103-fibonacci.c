86547d61547a646f7aca898cac5e37d13b94674b
German Franco F <933@holbertonschool.com>
2019-06-26 01:39:04 +0000

    logic change

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,11 +9,12 @@ int main(void)
 	long n2 = 2;
 	long sum = 0;
 	int i;
-	long sumOdd = 0;
+	long sumOdd = 2;
 
 	while (n2 < 4000000)
 	{
 		sum = n1 + n2;
+		n1 = n2;	
 		n2 = sum;
 		if (sum % 2 != 0)
 		{
