b96d43e143770eaad2f483f8311715d9f4426ea1
German Franco F <933@holbertonschool.com>
2019-06-27 15:00:15 +0000

    function that checks for a digit (0 through 9).

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* _isdigit - checks for lowercase character.
+* @c: The character to be checked
+* Return: 1 if c is a digit 0 otherwise
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
