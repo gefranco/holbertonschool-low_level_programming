027aa01426c1398801c2a0b50c9842081560e2ad
German Franco F <933@holbertonschool.com>
2019-06-27 19:22:10 +0000

    draws a diagonal line on the terminal.

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+* void print_diagonal - draws a diagonal line on the terminal
+* @n: size
+*/
+void print_diagonal(int n)
+{
+	int j;
+	int i;
+
+	for (j = 0; j < n ; j++)
+	{
+		for (i = 0; i < j ; i++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
