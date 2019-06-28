7e6588aea597eca8e130aa5269dbd2ff79bfd599
German Franco F <933@holbertonschool.com>
2019-06-28 05:43:18 +0000

    testing

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,32 +10,28 @@ int main(void)
 
 
 
-int totalPrimos=0;
 	
 	int long number = 612852475143;
 
  	int long found = number;
 
 	int long j;
-
-	int x;
 	int evaluate=0;	
 	
 	i = 2;		
 	
 
-//	printf("%li ", found);
 	
 		while( found >= 2)
 		{	
 					
 		evaluate = 0;	
 			
-			//	printf("%ld\t%ld \n", found, i);
+
 			for( j = i; j >=1; j -- )
 			{
 			
-			//	printf("%ld  \n", i);
+		
 				if( i % j == 0)
 				{
 					evaluate++;
@@ -54,13 +50,10 @@ int totalPrimos=0;
 			}
 			if(evaluate==2)
 			{
-				//printf("%ld SI ES PRIMO   %d \n", i,evaluate);
 				if(found % i != 0)
 				{
 					i++;
 					
-			//	printf("%ld \n", found);
-				//	evaluate = 0;
 				}
 				else{
 					while(found % i == 0)
@@ -68,23 +61,14 @@ int totalPrimos=0;
 						found = found / i;
 						printf("%ld, ",i );
 					
-					//totalPrimos ++;
 					}
 					i = 2;
 				}	
-				//ii=1;
-				/*for(x = 0 ; x > )
-				{
-					found = found / i;
-					printf("%ld\n ",found);
-				}*/
 				
 				
 			}else {
 			i++;
 }
-		//i++;
-		//i++;
 		}
 	
 	return (0);
