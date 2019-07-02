4fb4d78dc1f8eb796afd6c63ab7fe93fa8029099
German Franco F <933@holbertonschool.com>
2019-07-02 21:36:07 +0000

    copies the string pointed to by src

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,12 +13,11 @@ char *_strcpy(char *dest, char *src)
 
 	i = 0;
 
-	while ((c = *(src++)) != '\n')
+	while ((c = *(src++)))
 	{
 		dest[i] = c;
 		i++;
 	}
-	dest[i] = '\n';
-	dest[++i] = '\0';
+	
 	return (dest);
 }
