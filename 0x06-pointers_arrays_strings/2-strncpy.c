15fd3277c9223ddf08b729a381a45f1a1cc683a6
German Franco F <933@holbertonschool.com>
2019-07-04 22:16:42 +0000

    function that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strncpy - concatenate two strings
+ * @src :the array to be appended
+ * @dest : the array to append
+ * @n :the size of the array
+ * Return: the concatenated String
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while ((dest[i] = src[i]))
+	{
+		i++;
+		if (i == n)
+			break;
+	}
+	return (dest);
+}
