84a961485bae9a78c35096acbec9b7a004ce75bf
German Franco F <933@holbertonschool.com>
2019-07-04 19:49:00 +0000

    a function that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strcmp - concatenate two strings
+ * @s1 :the array to be appended
+ * @s2 : the array to append
+ * Return: the concatenated String
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int e;
+
+	i = 0;
+	e = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] > s2[i])
+		{
+			e = 15;
+			break;
+		}
+		else if (s2[i] > s1[i])
+		{
+			e = -15;
+			break;
+		}
+		i++;
+	}
+	return (e);
+}
