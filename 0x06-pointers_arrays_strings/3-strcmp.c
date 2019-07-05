8651ef83f9fc6206898ac1b2298b469704d73fe3
German Franco F <933@holbertonschool.com>
2019-07-05 03:39:46 +0000

    fix error

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,24 +10,20 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 	int e;
-	int i2;
 
 	i = 0;
 	e = 0;
-	i2=0;
 	while (s1[i] != '\0' || s2[i] != '\0')
 	{
 		if (s1[i] > s2[i])
 		{
-			e = 10;
-			i2 = i;
+			e = s1 - s2;
 		}
 		else if (s2[i] > s1[i])
 		{
-			e = -10;
-			i2 = i * -1;	
+			e = (s2 - s1)*(-1);
 		}
 		i++;
 	}
-	return (e + i2);
+	return (e);
 }
