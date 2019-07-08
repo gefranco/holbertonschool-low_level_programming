c79581f6e60a2d91f450eeec6a2268b5742381ce
German Franco F <933@holbertonschool.com>
2019-07-08 21:36:44 +0000

    fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _memset - fills memory with a constant byte
+ * @s :the array to fill
+ * @b :char to fill with
+ * @n :the number of memory to fill
+ * Return: the filled array
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n ; i++)
+	{
+		*s = b;
+	}
+	return (s);
+}
+
+
+
+
