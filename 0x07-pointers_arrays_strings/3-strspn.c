cf72c7b34b72b3e4d5a4882719b5878a3d974d78
German Franco F <933@holbertonschool.com>
2019-07-08 21:57:29 +0000

    gets the length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s :the array where find the segment
+ * @accept : the segment to find
+ * Return: total found
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int count;
+	int j;
+
+	i = 0;
+	count = 0;
+	while (s[i] != ' ')
+	{
+		j = 0;
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+			}
+			j++;
+		}
+
+		i++;
+	}
+	return (count);
+}
