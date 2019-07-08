687f52d61d960ccbc02d57e454aeeca56be1fa45
German Franco F <933@holbertonschool.com>
2019-07-08 21:53:07 +0000

    locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strchr -  locates a character in a string
+ * @s :the string where lloking the char
+ * @c: the character to find
+ * Return: the found string
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+	char toFind;
+
+	i = 0;
+	while ((toFind = s[i]))
+	{
+		if (toFind == c)
+			return (s + i);
+		i++;
+	}
+	return (NULL);
+}
+
+
+
+
