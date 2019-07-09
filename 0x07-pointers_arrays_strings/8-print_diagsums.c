7eee599bf31e2774532f4a9d9ee1545429f0b0ac
German Franco F <933@holbertonschool.com>
2019-07-09 01:42:28 +0000

    sets the value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints the sum of the two diagonals of a matrix
+ * @a :the array to be encoded
+ * @size: the size
+ * Return: the encoded string
+ */
+void print_diagsums(int *a, int size)
+{
+	int m;
+	int sum1;
+	int sum2;
+
+	sum1 = 0;
+	sum2 = 0;
+	for (m = 0; m < size ; m++)
+	{
+		sum1 += *(a + ((size + 1) * m));
+		sum2 += *(a + ((size - 1) * m));
+
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
