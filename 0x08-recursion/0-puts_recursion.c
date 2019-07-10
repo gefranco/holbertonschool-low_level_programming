ac0cb9af8cf08e0bbb2213ecd5415b58b76294c2
German Franco F <933@holbertonschool.com>
2019-07-10 19:53:34 +0000

    logic change

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,9 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	if (*++s != '\0')
-		_puts_recursion(s);
+	if (*s != '\0')
+	{	
+		
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
 	else
 		_putchar('\n');
 }
