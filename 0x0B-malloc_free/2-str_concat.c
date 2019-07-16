8a1be1b2f13d94dbc0b4216cd98bad3dbbcbaacc
German Franco F <933@holbertonschool.com>
2019-07-16 19:39:51 +0000

    function that concatenates two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - check if a char is a digit
+ * @s1: the first string
+ * @s2: the second string
+ * Return: the concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *a;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int i2 = 0;
+	unsigned int j2 = 0;
+
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
+	a = malloc((i + j) * sizeof(char));
+	while (i2 < i)
+	{
+		a[i2] = s1[i2];
+		i2++;
+	}
+
+	while (j2 < j)
+	{
+		a[j2 + i2] = s2[j2];
+		j2++;
+	}
+	return (a);
+}
+
