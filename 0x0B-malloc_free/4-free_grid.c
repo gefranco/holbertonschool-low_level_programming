35851960b468c2630536674684078e502ed3f4ab
German Franco F <933@holbertonschool.com>
2019-07-16 20:15:08 +0000

    a function that frees a 2 dimensional grid previously created

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,7 +4,7 @@
 /**
  * free_grid - check if a char is a digit
  * @grid: the multidimentional array
- * height: the height of the array
+ * @height: the height of the array
  * Return: 1 if is digit 0 otherwise
  */
 void free_grid(int **grid, int height)
