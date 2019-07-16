4f4bd9fd0471db223ab5d05bbc213bcad29589d1
German Franco F <933@holbertonschool.com>
2019-07-16 19:17:30 +0000

    returns a pointer to a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - check if a char is a digit
+ * @str: the char
+ * Return: 1 if is digit 0 otherwise
+ */
+char *_strdup(char *str)
+{
+	char *a;
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+
+	while (str[i])
+		i++;
+	a = malloc(i  * sizeof(char));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	while (j <= i)
+	{
+		a[j] = str[j];
+		j++;
+	}
+	return (a);
+}
