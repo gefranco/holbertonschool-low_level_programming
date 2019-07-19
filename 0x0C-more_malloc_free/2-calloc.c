cb276c3e884cda58f7c5cf43dc526821d9c866b4
German Franco F <933@holbertonschool.com>
2019-07-19 00:57:12 +0000

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,3 @@
-#include "holberton.h"
 #include <stdlib.h>
 /**
  * _calloc - a function that allocates memory for an array
@@ -15,12 +14,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	a =(void *) malloc(nmemb * size);
+	a = (void *) malloc(nmemb * size);
 	if (a == NULL)
 	{
 		return (NULL);
 	}
-	for (;i < nmemb;i++)
+	for ( ; i < nmemb; i++)
 	{
 	}
 	return (a);
