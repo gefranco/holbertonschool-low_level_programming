13b9af2ce0b69914e357a800f84a553bcafd3d1b
German Franco F <933@holbertonschool.com>
2019-07-22 21:33:42 +0000

    a function that frees dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "dog.h"
+/**
+ * free_dog -  function that frees dogs
+ * @d: the pointer to dog_t to free
+ */
 void free_dog(dog_t *d)
 {
-	free(d);
+	if (d)
+		free(d);
 }
