fc9ea13476dea6a7add97d2d33e79ec9154f2396
German Franco F <933@holbertonschool.com>
2019-07-22 20:35:50 +0000

    a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef mydog
+#define mydog
+/**
+ * struct dog - dog structure
+ * @name: Dog name
+ * @owner: dog owner
+ * @age: dog age
+ *
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
