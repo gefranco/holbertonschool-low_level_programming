90be392e003aeb5b54d67ed7050644bfde01680e
German Franco F <933@holbertonschool.com>
2019-07-23 00:36:13 +0000

    a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,8 +10,9 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	#define name name
+	#define owner owner
 	dog_t *dog1;
-	#define namec name
 	dog1 = malloc(sizeof(struct dog));
 	if (dog1 == NULL)
 		return (NULL);
