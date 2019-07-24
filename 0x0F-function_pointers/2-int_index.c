cea66279a6ce7469dc6757ee537f9bf59523330b
German Franco F <933@holbertonschool.com>
2019-07-24 18:53:15 +0000

    function that searches for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * cmp - check if a number is integer
+ * @n: the integer to check
+ *
+ * Return: 0 if true, 1 otherwise.
+ */
+int cmp(int n)
+{
+	if (n)
+		return (0);
+	else
+		return (1);
+}
+/**
+ * int_index - searches for an integer
+ * @array: the array
+ * @size: the size
+ * @cmp: the function
+ * Return: -1 if no number is found or size < 0, the number otherwise.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+	for (; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
