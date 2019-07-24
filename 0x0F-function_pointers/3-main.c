5cb5f05ab590451950aa1b988c56c4c26cf34410
German Franco F <933@holbertonschool.com>
2019-07-24 22:04:13 +0000

    a program that performs simple operations.

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "3-calc.h"
+int main(int argc, char *argv[])
+{
+
+	(void)argc;
+
+	printf("%d\n",get_op_func(argv[2])(atoi(argv[1]),atoi(argv[3])));
+
+	
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op_add - plus two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - sustract two numbers
+ * @a: The first number
+ * @b: The second number
+ * Return: the result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplication of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide two numbers
+ * @a: first number
+ * @b: second number
+ * Return:the result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Mod of two numbers
+ * @a: First number
+ * @b: Second number
+ * Return: the result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
