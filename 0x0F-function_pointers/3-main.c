5211a5e824e019916d87479c7d29514fd099e7a9
German Franco F <933@holbertonschool.com>
2019-07-24 22:08:08 +0000

    add comments

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,13 +2,15 @@
 #include<stdlib.h>
 #include<string.h>
 #include "3-calc.h"
+/**
+ * main - program entry point
+ * @argc: number of arguments
+ * @argv: the arguments
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
-
 	(void)argc;
-
-	printf("%d\n",get_op_func(argv[2])(atoi(argv[1]),atoi(argv[3])));
-
-	
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
