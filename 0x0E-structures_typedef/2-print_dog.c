64e9dae8830ddf397cf851adfc6902c8d9408aab
German Franco F <933@holbertonschool.com>
2019-07-24 00:35:12 +0000

    betty styles

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,13 +12,11 @@ void print_dog(struct dog *d)
 		if (!d->name)
 			d->name = "(nil)";
 		printf("Name: %s\n", d->name);
-		if(!d->age)
+		if (!d->age)
 			d->age = 0;
 		printf("Age: %f\n", d->age);
 		if (!d->owner)
 			d->owner = "(nil)";
-		printf("Owner: %s\n", d->owner);
-
-			
+		printf("Owner: %s\n", d->owner);	
 	}
 }
