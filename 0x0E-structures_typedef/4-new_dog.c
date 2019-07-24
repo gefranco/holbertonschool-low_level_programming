50bdcb1b8f423e643c5ed633752e6f322065675e
German Franco F <933@holbertonschool.com>
2019-07-24 00:53:10 +0000

    copy of the variables

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,15 +10,33 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	#define name name
-	#define owner owner
 	dog_t *dog1;
+	int i = 0;
+	int j = 0;
+	int i2 = 0;
+	int j2 = 0;
+	char *myname;
+	char *myowner;
 
 	dog1 = malloc(sizeof(struct dog));
 	if (dog1 == NULL)
 		return (NULL);
-	dog1->name = name;
+	while (name[i])
+		i++;
+	while (owner[j])
+		j++;
+	myname = malloc(sizeof(char) * i);
+	myowner = malloc(sizeof(char) * j);
+	for (; i2 < i; i2++)
+	{
+		myname[i2] = name[i2];
+	}
+	for (; j2 < j; j2++)
+	{
+		myowner[j2] = myowner[j2];
+	}
+	dog1->name = myname;
 	dog1->age = age;
-	dog1->owner = owner;
+	dog1->owner = myowner;
 	return (dog1);
 }
