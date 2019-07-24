8347f1e5d8bf3feaaa7801564ea36037455dda2b
German Franco F <933@holbertonschool.com>
2019-07-24 02:32:21 +0000

    correction copies

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,32 +11,12 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog1;
-	int i = 0;
-	int j = 0;
-	int i2 = 0;
-	int j2 = 0;
-	char *myname;
-	char *myowner;
 
 	dog1 = malloc(sizeof(struct dog));
 	if (dog1 == NULL)
 		return (NULL);
-	while (name[i])
-		i++;
-	while (owner[j])
-		j++;
-	myname = malloc(sizeof(char) * i);
-	myowner = malloc(sizeof(char) * j);
-	for (; i2 < i; i2++)
-	{
-		myname[i2] = name[i2];
-	}
-	for (; j2 < j; j2++)
-	{
-		myowner[j2] = owner[j2];
-	}
-	dog1->name = myname;
+	dog1->name = name;
 	dog1->age = age;
-	dog1->owner = myowner;
+	dog1->owner = owner;
 	return (dog1);
 }
