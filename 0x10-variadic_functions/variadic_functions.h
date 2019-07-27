68fc2f3b612f124199a0804e3735924c491c357e
German Franco F <933@holbertonschool.com>
2019-07-27 17:38:22 +0000

    header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,18 @@
+#ifndef vdf
+#define vdf
+#include <stdarg.h>
+	int sum_them_all(const unsigned int n, ...);
+	void print_numbers(const char *separator, const unsigned int n, ...);
+	void print_strings(const char *separator, const unsigned int n, ...);
+	void print_all(const char * const format, ...);
+
+	void print_char(va_list);
+	void print_int(va_list);
+	void print_float(va_list);
+	void print_str(va_list);
+	typedef struct prt
+	{
+		char *t;
+		void (*f)(va_list);
+	} prt_o;
+#endif
