9a8aa26c12c689fe2fe5da23c365833968e77479
German Franco F <933@holbertonschool.com>
2019-07-27 22:11:02 +0000

    assembler code hello

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,12 @@
-section .data
-    msg db "Hello, Holberton", 0x0a ; 0x0a = \n
-    .size equ $ - msg
+section .rodata
+    msg:    db 'Hello, Holberton', 14
+    msglen: equ $-msg
 
 section .text
-    global start
-    start:
-        mov rax, 0x2000004  ; syscall write
-        mov rdi, 1          ; standard out
-        mov rsi, msg        ; address of string
-        mov rdx, msg.size   ; size to write
-        syscall             ; invoke kernel
-        
-        mov eax, 0x2000001  ; syscall exit
-        mov rdi, 0          ; return code
-        syscall             ; invoke kernel
+    main:
+        ; write(1, msg, msglen)
+        mov rdi, 1
+        mov rsi, msg
+        mov rdx, msglen
+        mov rax, 1
+        syscall
