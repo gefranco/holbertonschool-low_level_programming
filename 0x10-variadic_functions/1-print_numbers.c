50a48b6995aff27ced5e6496ab4cd601d3cbe230
German Franco F <933@holbertonschool.com>
2019-07-27 19:54:42 +0000

    fix error

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,10 +14,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(valist, n);
 	for (i = 0; i < n - 1; i++)
 	{
-		if(separator)
+		if (separator)
 			printf("%d%s", va_arg(valist, int), separator);
 		else
-			printf("%d",va_arg(valist, int));
+			printf("%d", va_arg(valist, int));
 	}
 	printf("%d\n", va_arg(valist, int));
 	va_end(valist);
