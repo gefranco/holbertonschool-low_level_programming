44f7b230555756172f1dd1b5ca94099d3fa2662c
German Franco F <933@holbertonschool.com>
2019-07-27 21:51:13 +0000

    fix error

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,24 +1,25 @@
 #ifndef vdf
 #define vdf
 #include <stdarg.h>
-	int sum_them_all(const unsigned int n, ...);
-	void print_numbers(const char *separator, const unsigned int n, ...);
-	void print_strings(const char *separator, const unsigned int n, ...);
-	void print_all(const char * const format, ...);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
-	void print_char(va_list);
-	void print_int(va_list);
-	void print_float(va_list);
-	void print_str(va_list);
-	/**
-	* struct prt - Struct prt
-	*
-	* @t: The operator
-	* @f: The function associated
-	*/
-	typedef struct prt
-	{
-		char *t;
-		void (*f)(va_list);
-	} prt_o;
+void print_char(va_list);
+void print_int(va_list);
+void print_float(va_list);
+void print_str(va_list);
+/**
+ * struct print1 - struct of printers operators
+ * @t: First member
+ * @f: Second member
+ *
+ * Description: struct to manage diferent types of print
+ */
+typedef struct print1
+{
+	char *t;
+	void (*f)(va_list);
+} prt_o;
 #endif
