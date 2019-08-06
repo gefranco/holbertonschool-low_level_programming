7f7e4d28d45c05178f0bb461140cd8b4e9980d2c
German Franco F <933@holbertonschool.com>
2019-08-06 20:42:10 +0000

    [Fix]bug

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	/*while (h)*/
 	myhead = *head;
 
-	if (&*head == NULL)
+	if (&*head == NULL || *head == NULL)
 		return (NULL);
 
 	for (i = 1; i < idx ; i++)
