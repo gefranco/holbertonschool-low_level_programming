6bb7e44dba27299e7d913a728a7085c1b5eecc98
German Franco F <933@holbertonschool.com>
2019-08-14 01:21:01 +0000

    [Fix] a program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,40 +14,24 @@
 int copy_file(const char *src, const char *dest)
 
 {
-
 	int fd;
 	int fdd;
-
-
-	int j = 1024;
-
+	int j = 0;
 	int i;
 	char buf[1024];
+	fd = open(src, O_RDONLY);
 
-/*read*/
 
-	 fd = open(src, O_RDONLY);
-
-	 if (fd == -1)
-	 {
-		
-		 
-		 char *err = "Error: Can't read from file " + *src +'\n';
-		
-		 write(STDERR_FILENO, &err ,strlen(err));	 
-		
+       	if (fd == -1)
+	{		 
+		char *err = "Error: Can't read from file " + *src +'\n';
+		write(STDERR_FILENO, &err ,strlen(err));
 	       	exit(98);
-	 }
-	
-	
-
-
-
+	}
 	if (dest == NULL)
 		return (0);
 
 	fdd = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0661);
-
 	if (fdd == -1)
 	{
 		char *err = "Error: Can't write to " + *dest + '\n';
@@ -56,45 +40,29 @@ int copy_file(const char *src, const char *dest)
 		return (-1);
 	}
 
-
-
-	 read(fd, buf, 1024);
-
-	 while(buf[j] != '\0')
-	 {
-
-		 /*write(fdd, buf, 1024);*/
-		
-		 /*dprintf(fptr,"%d.%s\n", i, str);*/  
-		
-		 i = 0;
-		 while(buf[i] != '\0')
-		 {
-			/*write(fdd, buf, 1);*/
-			
-			 dprintf(fdd,"%c",buf[i]);
-			i++;
-		 }
-
+       	do
+	{
 		 read(fd, buf, 1024);
-		 j += 1024;
-	 }
-
+		 i = 0;
+		 while (buf[i] != '\0')
+		 {	
+		
+			 dprintf(fdd,"%c",buf[i]);
+			 i++;
+		 }
 		
 
+		 j += 1024;
+	 }while(buf[j] != '\0');
 
-
-
-	if(close(fd)==-1)
+	if (close(fd)==-1)
 	{
 		char *err = "Error: Can't close fd " + fd;
 		write(STDERR_FILENO,err,strlen(err));
 		exit(100);
-		
 	}
-		
 
-	if(close(fdd)==-1)
+	if (close(fdd)==-1)
 	{
 		char *err = "Error: Can't close fd  " + fd;
 		write(STDERR_FILENO, err,strlen(err));
@@ -120,43 +88,9 @@ int main(int argc, char *argv[])
 	}
 	
 
-copy_file(argv[1],argv[2]);
 
-/*	char buf[1024];
-	int fd;
+	return (copy_file(argv[1],argv[2]));
 
-
-	int i = 1024;
-
-	fd = open("Requiescat", O_RDONLY);
-
-	 if (fd == -1)
-	 {
-		 
-		 return(0);
-	 }
-
-	
-	read(fd, buf, 1024);	
-
-	 while(buf[i] != '\0')
-	 {
-		 read(fd, buf, 1024);
-	
-		 printf("%s", buf);
-		 i+=1024;
-	 }
-
-
-*/
-
-
-	 return (0);
 			 
 }
-
-
-
-
-
 
