5acb6f06c195c6df309ff205fcea3169ef4e62fb
German Franco F <933@holbertonschool.com>
2019-10-11 19:58:06 +0000

    [Fix]betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,33 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+/**
+ * hash_table_set - function that retrieves a value associated with a key
+ * @ht: the hash_table
+ * @key: the key
+ * @value:the value
+ * Return: Always EXIT_SUCCESS.
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 
 	unsigned long int ki = 0;
 	hash_node_t **myarray = ht->array;
-
 	hash_node_t *node;
-
 	hash_node_t *new_node;
-	
-	if(ht == NULL || key == NULL || strlen(key) == 0)
-                return 0;
 
+	if (ht == NULL || key == NULL || strlen(key) == 0)
+		return (0);
 	ki = key_index((unsigned char *)key, ht->size);
-
-	node = malloc(sizeof(struct hash_node_s*));
+	node = malloc(sizeof(struct hash_node_s *));
 	if (node == NULL)
 		return (0);
-
 	node->value = strdup(value);
-
 	node->key = strdup(key);
-
- 	node->next = NULL;
-
-	if(myarray[ki] == NULL)
+	node->next = NULL;
+	if (myarray[ki] == NULL)
 	{
 		myarray[ki] = node;
 		return (1);
@@ -36,23 +34,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		node = myarray[ki];
-		
-
-		new_node = malloc(sizeof(hash_node_t*));
+		new_node = malloc(sizeof(hash_node_t *));
 		if (new_node == NULL)
 			return (0);
-        	new_node->value = (char *)value;
-
-        	new_node->key = (char *)key;
-		
+		new_node->value = (char *)value;
+		new_node->key = (char *)key;
 		new_node->next = node;
-
 		myarray[ki] = new_node;
-
 		new_node = myarray[ki];
 
-
 	}
-
 	return (1);
 }
