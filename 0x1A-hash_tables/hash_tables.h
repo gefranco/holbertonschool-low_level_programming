6899a0699ee516bc1c8ce8240665e8d755113fa3
German Franco F <933@holbertonschool.com>
2019-10-11 16:18:51 +0000

    [Fix]

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,13 @@
 #ifndef hstbl
 #define hstbl
-
+/**
+ * struct hash_node_s - Node of a hash table
+ *
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ */
 typedef struct hash_node_s
 {
 	char *key;
@@ -8,7 +15,14 @@ typedef struct hash_node_s
 	struct hash_node_s *next;
 } hash_node_t;
 
-
+/**
+ * struct hash_table_s - Hash table data structure
+ *
+ * @size: The size of the array
+ * @array: An array of size @size
+ * Each cell of this array is a pointer to the first node of a linked list,
+ * because we want our HashTable to use a Chaining collision handling
+ */
 typedef struct hash_table_s
 {
 	unsigned long int size;
