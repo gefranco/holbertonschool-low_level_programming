7a250774fc7a685b5ae2a6dde53b30b38061edc3
German Franco F <933@holbertonschool.com>
2019-10-11 19:09:06 +0000

    [Fix]

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,6 +4,8 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 
+	if(ht == NULL || key == NULL)
+		return 0;
 	unsigned long int ki = 0;
 	hash_node_t **myarray = ht->array;
 
@@ -13,15 +15,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	ki = key_index((unsigned char *)key, ht->size);
 
-	node = malloc(sizeof(struct hash_node_s));
+	node = malloc(sizeof(struct hash_node_s*));
+	if (node == NULL)
+		return (0)
 
 	node->value = (char *)value;
 
 	node->key = (char *)key;
 
  	node->next = NULL;
-
-
 
 	if(myarray[ki] == NULL)
 	{
@@ -33,7 +35,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		node = myarray[ki];
 		 
 		new_node = malloc(sizeof(hash_node_t*));
-
+		if (new_node == NULL)
+			return (0);
         	new_node->value = (char *)value;
 
         	new_node->key = (char *)key;
