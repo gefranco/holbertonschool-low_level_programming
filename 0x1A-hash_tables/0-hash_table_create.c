8c03ade8a8db1b5b1b5908266928d3231b17ef9a
German Franco F <933@holbertonschool.com>
2019-10-12 03:45:09 +0000

    [Fix]

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,21 +10,19 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 
-	hash_node_t **array;
 	hash_table_t *hash_table;
 
 
 	if (size < 1)
 		return (NULL);
-	array = malloc(size * sizeof(hash_node_t *));
-	if (array == NULL)
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
 		return (NULL);
 
-	hash_table = malloc(sizeof(hash_table_t));
-		if (hash_table == NULL)
-			return (NULL);
 	hash_table->size = size;
-	hash_table->array = array;
 	return (hash_table);
 
 }
