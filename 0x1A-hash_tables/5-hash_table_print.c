f351569166308df2aeabf40eed059dd5254226e2
German Franco F <933@holbertonschool.com>
2019-10-12 01:27:37 +0000

    [Fix]

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,14 +8,13 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int size;
-	int printed = 0;
-	unsigned long int i, contador = 0;
+	unsigned long int i, size, contador = 0;
 	hash_node_t *node, *tmp_node, **myarray;
-	if (ht == NULL || ht->array == NULL || ht->size<1)
-        {
-                return;
-        }
+
+	if (ht == NULL || ht->array == NULL || ht->size < 1)
+	{
+		return;
+	}
 	size = ht->size;
 	myarray = malloc(ht->size * sizeof(hash_node_t));
 	printf("{");
@@ -23,20 +22,10 @@ void hash_table_print(const hash_table_t *ht)
 	{
 		node = ht->array[i];
 		if (node && node->next == NULL)
-			if (printed == 0)
-			{
-				printed = 1;
-				myarray[contador] = node;
-				contador++;
-
-				
-			}
-			else
-			{
-							
-				myarray[contador] = node;
-				contador++;
-			}
+		{
+			myarray[contador] = node;
+			contador++;
+		}
 		else if (node)
 		{
 			tmp_node = node;
@@ -46,16 +35,12 @@ void hash_table_print(const hash_table_t *ht)
 				contador++;
 				tmp_node = tmp_node->next;
 			}
-
 		}
-
-
 	}
-	for(i = 0; i < contador;i++)
-		if(i == contador -1)	
+	for (i = 0; i < contador; i++)
+		if (i == contador - 1)
 			printf("'%s': '%s'", (char *)myarray[i]->key, (char *)myarray[i]->value);
 		else
 			printf("'%s': '%s', ", (char *)myarray[i]->key, (char *)myarray[i]->value);
-
 	printf("}\n");
 }
