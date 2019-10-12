f365994f32539d13aaa5620e8cb6fdf9fad1ca33
German Franco F <933@holbertonschool.com>
2019-10-12 04:31:59 +0000

    [Fix]

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,7 +9,7 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i, size;
-	hash_node_t *node, *tmp_node;
+	hash_node_t  *tmp_node;
 
 	int comma = 0;
 
@@ -22,18 +22,17 @@ void hash_table_print(const hash_table_t *ht)
 
 	for (i = 0; i < size; i++)
 	{
-		node = ht->array[i];
-			tmp_node = node;
-			while (tmp_node != NULL)
-			{
-				if (comma == 1)
-					printf(", ");
-				printf("'%s': '%s'", tmp_node->key, tmp_node->value);
-				comma = 1;
+		tmp_node = ht->array[i];
+		while (tmp_node != NULL)
+		{
+			if (comma == 1)
+				printf(", ");
+			printf("'%s': '%s'", tmp_node->key, tmp_node->value);
+			comma = 1;
 
 
-				tmp_node = tmp_node->next;
-			}
+			tmp_node = tmp_node->next;
+		}
 
 	}
 	printf("}\n");
