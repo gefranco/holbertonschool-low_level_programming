ac90954386c8acc9293ff4d0e4f78b1f886b61de
German Franco F <933@holbertonschool.com>
2019-10-12 15:10:38 +0000

    [fix]key index function

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -11,7 +11,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index = 0;
 
-	index = (hash_djb2(key)) & (size - 1);
+	index = (hash_djb2(key)) % (size);
 
 
 	return (index);
