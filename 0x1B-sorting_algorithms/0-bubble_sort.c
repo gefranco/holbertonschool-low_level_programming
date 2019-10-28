b27a94d60491ef5c05b3841f34c89451a323d784
German Franco F <933@holbertonschool.com>
2019-10-28 19:53:01 +0000

    [fix]

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -5,11 +5,11 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int tmp;
 
-	for (i = 0; i < size -1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-	
+
 		for (j = 0; j < size - 1 ; j++)
-		{	
+		{
 			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
@@ -19,7 +19,7 @@ void bubble_sort(int *array, size_t size)
 				print_array(array, size);
 
 			}
-		}	
+		}
 
 	}
 
