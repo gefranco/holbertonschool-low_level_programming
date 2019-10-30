2a026db11b6dd31ffbaa45ee75931b0f470081d1
German Franco F <933@holbertonschool.com>
2019-10-30 15:19:45 +0000

    [Fix] betty insertion-sort-list

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -1,180 +1,90 @@
 #include "sort.h"
 
-void swap(listint_t *listj);
+/**
+ * insertion_sort_list - sorts a doubly linked list (Insertion sort algorithm)
+ * @list: pointer to the head of the list
+ * Return: Always 0
+ */
 void insertion_sort_list(listint_t **list)
 {
-	
 	listint_t *listj = *list;
 	listint_t *listprev, *tmp;
+
 	while (listj->next)
 	{
-		if ((listj->next->n < listj->n))
+		if (listj->next->n < listj->n)
 		{
-			/*
-			 * swap
-			 * go back until num <
-			 * */
-			
-			
-			
 			swap(listj);
-			
-			print_list(*list);	
-			
+			print_list(*list);
 			listprev = listj;
-
-
 			while (listprev)
 			{
-				if(listprev->prev && listprev->prev->n > listprev->n)
+				if (listprev->prev && listprev->prev->n > listprev->n)
 				{
-					/*if(listprev->prev->prev)*/
-					if (listprev->prev->prev==NULL)
+					if (listprev->prev->prev == NULL)
 					{
-						listprev->prev->next=listprev->next;
+						listprev->prev->next = listprev->next;
 						listprev->next->prev = listprev->prev;
-
 						listprev->prev->prev = listprev;
-						tmp=listprev->prev;
-						listprev->next= tmp;
-						
-						listprev->prev=NULL;
-
+						tmp = listprev->prev;
+						listprev->next = tmp;
+						listprev->prev = NULL;
 						*list = listprev;
-						/*print_list(listprev);*/
 					}
-
-					else if(listprev->prev->prev)
+					else if (listprev->prev->prev)
 						swap(listprev->prev);
 					else
 						listprev = listprev->prev;
-
 				print_list(*list);
 				}
 				else
 					listprev = listprev->prev;
-
-				/*print_list(*list);*/
-					
 			}
-
-			/*print_list(*list);*/
-			/*if (listj->next->next)
-			{
-				prev = listj->next;
-				listj->prev->next = listj->next;
-
-				listj->next->prev = listj->prev;
-
-				tmp = listj->next->next;
-				listj->next->next = listj;
-				listj->next = tmp;
-
-				listj->next->prev = listj;
-				listj->prev = prev;
-
-			}
-			else
-			{
-				tmp = listj->next;
-                                listj->prev->next = tmp;
-				listj->next->next = listj;
-				listj->next = NULL;
-			}*/
 		}
-
-
-		else	
+		else
 			listj = listj->next;
-
-
-		/*print_list(*list);*/
 	}
 }
 
 
-
+/**
+ * swap - swap two nodes from a doubly linked list
+ * @listj: header
+ */
 void swap(listint_t *listj)
 {
-	
 	listint_t *tmp, *prev;
-		if ((listj->next->n < listj->n))
+		if (listj->next->n < listj->n)
 		{
-			/*
-			 * swap
-			 * go back until num <
-			 * */
-
-
 			if (listj->next->next)
 			{
 				prev = listj->next;
-			
-		      /*		listj->next->prev = listj->prev;
-
-                                tmp = listj->next->next;
-                                listj->next->next = listj;
-                                listj->next = tmp;
-
-                                listj->next->prev = listj;
-			*/
-				if(listj->prev)
-				{	
+				if (listj->prev)
+				{
 					listj->prev->next = listj->next;
-
-
-				/*listj->next->prev = listj->prev;
-
-                                tmp = listj->next->next;
-                                listj->next->next = listj;
-                                listj->next = tmp;
-
-                                listj->next->prev = listj;
-*/
-		/*		listj->prev = prev;*/
 				}
 				else
 				{
-				/*	listj = listj->next;
-					print_list(listj);*/
-					/*listj=listj->next;
-					listj->prev=NULL;*/
-					/*listj->prev = 
-					listj->prev = tmp;
-					listj->prev->prev = NULL;
-				*/
 
-				}	
-
+				}
 				listj->next->prev = listj->prev;
-
 				tmp = listj->next->next;
 				listj->next->next = listj;
 				listj->next = tmp;
 
 				listj->next->prev = listj;
-			if(listj->prev)
+				if (listj->prev)
 					listj->prev = prev;
-			
-				
 
 			}
 			else
 			{
 				tmp = listj->next;
-                                
 				listj->prev->next = tmp;
-
 				listj->next->prev = listj->prev;
-								
 				listj->prev = listj->next;
-
 				listj->next->next = listj;
-			
 				listj->next = NULL;
 			}
 		}
-
-
 }
-
