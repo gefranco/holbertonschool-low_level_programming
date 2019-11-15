fc6f70ae04fcfd4ae5b3ba756d6e06ba4a8bc971
German Franco F <933@holbertonschool.com>
2019-11-15 23:26:47 +0000

    [Add] pep8

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,18 +4,20 @@
 module that provides island_perimeter function
 ===============
 """
+
+
 def island_perimeter(grid):
     """
     Return island perimeter
     """
-    
+
     island_v = 0
     island_va = 0
     island_h = 0
     for column in grid:
         island_v = 0
         for x in column:
-            if (x==1):
+            if x == 1:
                 island_v += 1
         if (island_v == 1):
             island_h += 1
@@ -23,6 +25,4 @@ def island_perimeter(grid):
             island_va += island_v
     print(island_va)
     print(island_h)
-
-
     return island_va * island_h * 2
