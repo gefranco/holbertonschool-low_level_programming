c491108abe594a18cb1f02644e6d52f6beff49d1
German Franco F <933@holbertonschool.com>
2019-12-05 14:59:00 +0000

    [fix]return NULL when fails

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -12,11 +12,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	binary_tree_t *node;
 
-	if (parent == NULL)
-		return (NULL);
 
 	node = malloc(sizeof(binary_tree_t));
-
+	if (node == NULL)
+		return (NULL);
 	node->parent = parent;
 
 	node->n = value;
