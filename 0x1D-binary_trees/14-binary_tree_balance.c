0b593d78a06a34fb8810345e901bb83207add3c1
German Franco F <933@holbertonschool.com>
2019-12-05 16:24:26 +0000

    [fix]

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -11,11 +11,13 @@ int tree_height(const binary_tree_t *tree);
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left = 0;
+	int right = 0;
+
 	if (tree == NULL)
 		return (0);
-	int left = tree_height(tree->left) + 1;
-	int right = tree_height(tree->right) + 1;
-
+	tree_height(tree->left) + 1;
+	tree_height(tree->right) + 1;
 	return (left - right);
 }
 /**
