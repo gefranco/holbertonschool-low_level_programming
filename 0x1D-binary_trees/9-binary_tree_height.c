6abda1dfbe898022ae1872c3c429e88b24b51c1a
German Franco F <933@holbertonschool.com>
2019-12-05 15:53:33 +0000

    [add] function that measures the height of a binary tree

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -3,38 +3,36 @@
 
 size_t max_number(size_t num1, size_t num2);
 
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root
+ * Return: the height
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 
 
+	if (tree == NULL)
+		return (0);
 
-	 if(tree == NULL)
-                return 0;
+	else if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-        else if(tree->left == NULL && tree->right == NULL)
-                return(0);
+	return (max_number(binary_tree_height
+			(tree->left), binary_tree_height(tree->right)) + 1);
 
-        return max_number(binary_tree_height(tree->left), binary_tree_height(tree->right)) + 1;
-	
 }
 
-/*
-size_t count_height(const binary_tree_t *tree)
-{
-	if(tree == NULL)
-		return 0;
-	
-	else if(tree->left == NULL && tree->right == NULL)
-		return(0);
-	
-	return max_number(count_height(tree->left), count_height(tree->right)) + 1;
-	
-}
-*/
+/**
+ * max_number - return the max number
+ * @num1: number 1
+ * @num2: number 2
+ * Return: the max number
+ */
 size_t max_number(size_t num1, size_t num2)
 {
 	if (num1 > num2)
-		return num1;
+		return (num1);
 	else
-		return num2;
+		return (num2);
 }
