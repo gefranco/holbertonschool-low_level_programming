6feacec2417dbf980c37bc1c1dcf6006baebb817
German Franco F <933@holbertonschool.com>
2020-01-15 19:39:34 +0000

    [add]comments, betty style

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include "search_algos.h"
 
+/**
+ * linear_search - Linear search algorithm to find a value
+ * @array: the array
+ * @size: the array size
+ * @value: the value to find
+ * Return: The index of the value if found, -1 otherwise
+ */
 int linear_search(int *array, size_t size, int value)
 {
 
@@ -9,7 +16,10 @@ int linear_search(int *array, size_t size, int value)
 	size_t i;
 	(void) size;
 	(void) value;
-	
+
+	if (array == NULL)
+		return (-1);
+
 	array[size] = value;
 	for (i = 0;  ; i++)
 	{
